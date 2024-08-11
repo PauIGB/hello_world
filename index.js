278539c18ec5f601d68b08f9067e2e4a6291aa41
Pavel Karpov <pavel.karpov@myoffice.team>
2024-08-11 10:35:00 +0300

    upd

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+// ==UserScript==
+// @name              Hamster Kombat Web
+// @namespace         Hamster Kombat
+// @version           1.4
+// @description       Launch Hamster Kombat in browser
+// @description:ru-RU Запуск Hamster Kombat в браузере
+// @match             *://*.hamsterkombat.io/*
+// @match             *://*.hamsterkombatgame.io/*
+// @match             https://github.com/mudachyo/Hamster-Kombat
+// @grant none
+// @icon              https://hamsterkombatgame.io/images/icons/hamster-coin.png
+// @downloadURL       https://raw.githubusercontent.com/PauIGB/hello_world/main/index.js
+// ==/UserScript==
+
+console.log('hello world')
